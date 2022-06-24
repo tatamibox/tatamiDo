34ee6f26da77d0a5b591cb5e464d3c14f835c569
tatamibox <tat4mi@gmail.com>
2022-06-24 15:44:10 -0700

    create full project adding mechanism

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ class Project {
 const nameProject = document.querySelector('.nameProject');
 const nameInput = document.querySelector('.input');
 const nameCancel = document.querySelector('.nameCancel');
+const userInput = document.querySelector('.userInput');
+const nameAdd = document.querySelector('.nameAdd');
+const projectList = document.querySelector('.projectList');
 
 nameProject.addEventListener('click', () => {
     nameInput.style.display = 'inline';
@@ -17,3 +20,13 @@ nameProject.addEventListener('click', () => {
 nameCancel.addEventListener('click', () => {
     nameInput.style.display = 'none';
 })
+
+nameAdd.addEventListener('click', () => {
+    const thisProject = new Project(userInput.value);
+    const newListing = document.createElement('li');
+    newListing.innerHTML = thisProject.name;
+    projectList.appendChild(newListing);
+    userInput.value = '';
+    nameInput.style.display = 'none';
+})
+
