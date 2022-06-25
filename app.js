1fafdaa03a492aa8c2461e6d4867bf6bab35e517
tatamibox <tat4mi@gmail.com>
2022-06-24 17:05:16 -0700

    added task display mechanism, need to clean code

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ class Project {
         this.name = name;
     }
 }
+
+class Task {
+    constructor(objective, date) {
+        this.objective = objective;
+        this.date = date;
+    }
+}
 //
 // all naming project inputs
 const nameProject = document.querySelector('.nameProject');
@@ -28,6 +35,66 @@ nameAdd.addEventListener('click', () => {
     const newListing = document.createElement('li');
     newListing.innerHTML = thisProject.name;
     const newListingDiv = document.createElement('div');
+    const addNewTask = document.createElement('button');
+    const addTaskForm = document.createElement('form');
+    const addTaskInput = document.createElement('input');
+    addTaskInput.classList.add('taskInput');
+    const taskFormCancel = document.createElement('button');
+    taskFormCancel.innerHTML = 'Cancel';
+    taskFormCancel.classList.add('btn');
+    taskFormCancel.classList.add('btn-danger');
+    taskFormCancel.type = 'button';
+    taskFormCancel.addEventListener('click', () => {
+        addTaskForm.style.display = 'none';
+        addNewTask.style.display = 'inline';
+    })
+
+    const taskList = [];
+
+    const taskContainer = document.createElement('div');
+
+
+
+    const taskFormSubmit = document.createElement('button');
+    taskFormSubmit.classList.add('btn');
+    taskFormSubmit.classList.add('btn-success');
+    taskFormSubmit.type = 'button';
+    taskFormSubmit.innerHTML = 'Submit';
+    taskFormSubmit.addEventListener('click', () => {
+        taskContainer.innerHTML = '';
+
+        const newTask = new Task(addTaskInput.value, 'hello');
+        taskList.push(newTask);
+        for (let i = 0; i <= taskList.length - 1; i++) {
+            const taskDiv = document.createElement('div');
+            taskDiv.innerHTML = taskList[i].objective;
+            taskContainer.appendChild(taskDiv);
+        }
+        newListingDiv.appendChild(taskContainer);
+    })
+
+
+    addTaskForm.appendChild(addTaskInput);
+    addTaskInput.placeholder = 'What is the task?';
+    addTaskForm.style.display = 'none';
+    addTaskForm.appendChild(taskFormCancel);
+    addTaskForm.appendChild(taskFormSubmit);
+    newListingDiv.appendChild(addNewTask);
+    newListingDiv.appendChild(addTaskForm);
+
+
+
+
+
+
+
+
+    addNewTask.innerHTML = 'Add task';
+    addNewTask.addEventListener('click', () => {
+        addTaskForm.style.display = 'inline';
+        addNewTask.style.display = 'none';
+    })
+
 
 
     newListing.addEventListener('click', () => {
